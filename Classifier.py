a2bdb03cc469bea9523e2d2a1d7b6afc2cb94ce7
PISK12 <piotr.k14@gmail.com>
2018-03-31 21:18:42 +0200

    Add files via upload

--- Classifier.py
+++ Classifier.py
@@ -2,15 +2,18 @@ import shutil
 import os
 import time
 import json
+import datetime
 
 CONFIG_FILE = "classifier"
 
 FILE_EXTENSION = {"Media": (os.path.join(os.getcwd(), "Media"), ("avi", "mkv", "mp4", "mp3", "pdf",
                                                                  "txt", "epub", "doc", "csv", "xml", "wav", "jpg", "wma", "bmp")),
                   "Other": (os.path.join(os.getcwd(), "Other"), ("exe", "zip", "7z", "iso"))}
-TIME = "TIME"
+SCAN_TIME = "SCAN_TIME"
 FLAG_FILE = "FLAG_FILE"
 ONLY_ONCE = "ONLY_ONCE"
+FILE_TIME = "FILE_TIME"
+TYPE_TIME = "TYPE_TIME"
 
 
 def ReadConfigFile():
@@ -21,7 +24,7 @@ def ReadConfigFile():
 def createCofigFile():
     with open(CONFIG_FILE, "w") as file:
         file.write(json.dumps(
-            {"FILE_EXTENSION": FILE_EXTENSION, "TIME": 1, "FLAG_FILE": True, "ONLY_ONCE": False}))
+            {"FILE_EXTENSION": FILE_EXTENSION, SCAN_TIME: 1, FLAG_FILE: True, ONLY_ONCE: False, FILE_TIME: 10, TYPE_TIME: 60}))
 
 
 def main():
@@ -34,6 +37,7 @@ def main():
         if not os.path.exists(configFile["FILE_EXTENSION"][directory][0]):
             os.makedirs(configFile["FILE_EXTENSION"][directory][0])
 
+    cash = {}
     if flag:
         if not os.path.exists(FLAG_FILE):
             with open(FLAG_FILE, "w") as file:
@@ -46,17 +50,24 @@ def main():
         if flag:
             if not os.path.exists(FLAG_FILE):
                 break
+        time_now = datetime.datetime.now().replace(microsecond=0)
         for file in os.listdir():
-            if "." in file:
-                f = file.split(".")
-                f, extension = "".join(f[:-1]), f[-1]
-                for directory in configFile["FILE_EXTENSION"]:
-                    if extension in configFile["FILE_EXTENSION"][directory][1]:
-                        shutil.move(
-                            file, configFile["FILE_EXTENSION"][directory][0])
+            if file in cash:
+                if (datetime.datetime.now() - cash[file]).seconds > configFile[FILE_TIME] * configFile[TYPE_TIME]:
+                    if "." in file:
+                        f = file.split(".")
+                        f, extension = "".join(f[:-1]), f[-1]
+                        for directory in configFile["FILE_EXTENSION"]:
+                            if extension in configFile["FILE_EXTENSION"][directory][1]:
+                                shutil.move(
+                                    file, configFile["FILE_EXTENSION"][directory][0])
+
+            else:
+                cash[file] = datetime.datetime.now()
+
         if configFile[ONLY_ONCE]:
             break
-        time.sleep(configFile[TIME])
+        time.sleep(configFile[SCAN_TIME] * configFile[TYPE_TIME])
 
 
 if __name__ == '__main__':
