f0b4a6cc030ff918abeeb8858737c6e5dc3026d4
PISK12 <piotr.k14@gmail.com>
2018-03-31 20:19:28 +0200

    new version
    add choice place for directory

--- Classifier.py
+++ Classifier.py
@@ -5,8 +5,9 @@ import json
 
 CONFIG_FILE = "classifier"
 
-FILE_EXTENSION = {"Media": ("avi", "mkv", "mp4", "mp3", "pdf",
-                            "txt", "epub", "doc", "csv", "xml", "wav", "jpg", "wma", "bmp"), "Other": ("exe", "zip", "7z", "iso"), }
+FILE_EXTENSION = {"Media": (os.path.join(os.getcwd(), "Media"), ("avi", "mkv", "mp4", "mp3", "pdf",
+                                                                 "txt", "epub", "doc", "csv", "xml", "wav", "jpg", "wma", "bmp")),
+                  "Other": (os.path.join(os.getcwd(), "Other"), ("exe", "zip", "7z", "iso"))}
 TIME = "TIME"
 FLAG_FILE = "FLAG_FILE"
 ONLY_ONCE = "ONLY_ONCE"
@@ -30,8 +31,8 @@ def main():
     flag = configFile[FLAG_FILE]
 
     for directory in configFile["FILE_EXTENSION"]:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+        if not os.path.exists(configFile["FILE_EXTENSION"][directory][0]):
+            os.makedirs(configFile["FILE_EXTENSION"][directory][0])
 
     if flag:
         if not os.path.exists(FLAG_FILE):
@@ -50,9 +51,9 @@ def main():
                 f = file.split(".")
                 f, extension = "".join(f[:-1]), f[-1]
                 for directory in configFile["FILE_EXTENSION"]:
-                    if extension in configFile["FILE_EXTENSION"][directory]:
-                        shutil.move(file,
-                                    os.path.join(os.getcwd(), directory))
+                    if extension in configFile["FILE_EXTENSION"][directory][1]:
+                        shutil.move(
+                            file, configFile["FILE_EXTENSION"][directory][0])
         if configFile[ONLY_ONCE]:
             break
         time.sleep(configFile[TIME])
